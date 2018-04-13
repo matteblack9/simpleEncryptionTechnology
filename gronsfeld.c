e808859608e7203afbb27630f255a9e4e7f88433
현우 김 <a0nesthetic@pusan.ac.kr>
2018-04-13 17:35:42 +0900

    Modification of Gronsfeld technology

--- gronsfeld.c
+++ gronsfeld.c
@@ -7,24 +7,24 @@ int main(){
     int Key_Size = 0, str_size = 0, select = 1;
     char str[50] = {0, }, key[16] = {0, };
 
-    printf("암호문 또는 평문 입력: ");
+    printf("Enter plain text or Cipher text: ");
     scanf("%s", str);
 wrong:
-    printf("암호화는 1번, 복호화는 2번 선택: ");
+    printf("1 - Encryption 2 - Decryption\nChoose the work : ");
     scanf("%d", &select);
     if(select == 1 || select == 2) ;
     else goto wrong;
     fflush(stdin);
-    printf("키 값 입력: ");
+    printf("Enter the Key : ");
     scanf("%s", key);
     
     str_size = strlen(str);
     Key_Size = strlen(key);
 
     gronsfeld(str, key, str_size, Key_Size, select);
-
-    if(select == 1) printf("암호화된 키: %s", str);
-    if(select == 2) printf("복호화된 키: %s", str);
+    printf("\n");
+    if(select == 1) printf("Encrypted Key: %s", str);
+    if(select == 2) printf("Decrypted Key: %s", str);
     
     return 0;
 }
@@ -35,34 +35,39 @@ void gronsfeld(char* str, char* key, int str_size, int Key_Size, int select){
     for(i = 0,j = 0; i < str_size; i++, j++){
         j = i % Key_Size;
 
+        int isUcharacter = ((str[i] >= 'a') && (str[i] <= 'z'));
+        int isLcharacter = ((str[i] >= 'A') && (str[i] <= 'Z'));
+        int isnumber = ((str[i] >= '0') && (str[i] <= '9'));
+
         if(select == 1){
-            if((str[i] >= 'a') && (str[i] <= 'z')){
-                str[i] -= 'a'; temp = key[j] - '0';
-                if(str[i] + temp < 0) str[i] += 26;
+            if(isUcharacter || isLcharacter || isnumber) {
+               if(isUcharacter) str[i] -= 'a'; 
+               else if(isLcharacter) str[i] -= 'A'; 
+               else if(isnumber) str[i] -= '0';
+                temp = key[j] - '0';
+                while(str[i] + temp < 0) {
+                    str[i] += 26;
+                }
+
                 str[i] = (str[i] + temp) % 26;
-                str[i] += 'a';
+               if(isUcharacter) str[i] += 'a'; 
+               else if(isLcharacter) str[i] += 'A';
+               else if(isnumber) str[i] += '0';
             }
-            else if((str[i] >= 'A') && (str[i] <= 'Z')){
-                str[i] -= 'A'; temp = key[j] - '0';
-                if(str[i] + temp < 0) str[i] += 26;
-                str[i] = (str[i] + temp) % 26;
-                str[i] += 'A';
-            }  
         }
 
         else if(select == 2){
-            if((str[i] >= 'a') && (str[i] <= 'z')){
-                str[i] -= 'a'; temp = key[j] - '0';
-                if(str[i] - temp < 0) str[i] += 26;
+            if(isUcharacter || isLcharacter || isnumber){
+                if(isUcharacter) str[i] -= 'a'; 
+                else if(isLcharacter) str[i] -= 'A'; 
+                else if(isnumber) str[i] -= '0';
+                temp = key[j] - '0';
+                while(str[i] - temp < 0) str[i] += 26;
                 str[i] = (str[i] - temp) % 26;
-                str[i] += 'a';
+                if(isUcharacter) str[i] += 'a'; 
+                else if(isLcharacter) str[i] += 'A'; 
+                else if(isnumber) str[i] += '0';
             }
-            else if((str[i] >= 'A') && (str[i] <= 'Z')){
-                str[i] -= 'A'; temp = key[j] - '0';
-                if(str[i] - temp < 0) str[i] += 26;
-                str[i] = (str[i] - temp) % 26;
-                str[i] += 'A';
-            }  
         }
         else ;
     }
